93b24eb9f7b2523268ebcd92c306f81a3b86ac87
brightiup <zhuoliang3637@gmail.com>
2018-01-12 19:07:05 +0800

    del comm

--- kqueue.py
+++ kqueue.py
@@ -8,7 +8,6 @@ def kqueue(debugger, command, result, internal_dict):
         return 
     lldb_command = "p *(struct kqueue*)((*p->p_fd->fd_ofiles + %s)->f_fglob->fg_data)"%command
     debugger.HandleCommand(lldb_command)
-    # print >>result, "Breakpoint set on kqueue on the condition of procrss id is %s"%command
 
 def __lldb_init_module(debugger, internal_dict):
     debugger.HandleCommand("command script add kqueue -f kqueue.kqueue")
